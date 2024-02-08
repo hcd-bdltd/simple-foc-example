c2ce814cd3296bdca57c0fcd7f7f0506cdee34a6
Hudson C. Dalpra <hudson@bduncanltd.com>
2024-02-08 13:54:43 +1300

    feat(hall-sensor): added HALL sensor example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,50 @@
-#include <Arduino.h>
+/**
+ *  Hall sensor example code
+ *
+ * This is a code intended to test the hall sensors connections
+ * and to demonstrate the hall sensor setup.
+ *
+ */
 
-float target = 0.0;
+#include <SimpleFOC.h>
 
-void serial_loop(void) {
-        static String str;
+#define POLE_PAIRS 7
 
-        while (Serial.available()) {
-                char ch = (char) Serial.read();
-                str += ch;
+// Hall sensor instance
+// HallSensor(int hallA, int hallB , int hallC, int pp)
+//  - hallA, hallB, hallC    - HallSensor A, B and C pins
+//  - pp                     - pole pairs
+HallSensor sensor = HallSensor(A_HALL1, A_HALL2, A_HALL3, POLE_PAIRS);
 
-                if (ch == '\n')
-                {
-                        target = str.toFloat();
-                        Serial.print("Target: ");
-                        Serial.println(target);
-                        str = "";
-                }
-
-        }
-}
+// Interrupt routine initialisation
+// channel A, B and C callbacks
+void doA(){sensor.handleA();}
+void doB(){sensor.handleB();}
+void doC(){sensor.handleC();}
 
 void setup() {
+        // monitoring port
         Serial.begin(115200);
+
+        // check if you need internal pullup
+        sensor.pullup = Pullup::USE_EXTERN;
+
+        // initialise encoder hardware
+        sensor.init();
+        // hardware interrupt enable
+        sensor.enableInterrupts(doA, doB, doC);
+
+        Serial.println("Sensor ready");
+        _delay(1000);
 }
 
 void loop() {
-        serial_loop();
+        // iterative function updating the sensor internal variables
+        // it is usually called in motor.loopFOC()
+        sensor.update();
+        // display the angle and the angular velocity to the terminal
+        Serial.print(sensor.getAngle());
+        Serial.print("\t");
+        Serial.println(sensor.getVelocity());
+        delay(100);
 }
